a27e9913a134b28819ca5614fe921e6759bdea6a
yukthayandrapu <2420080060@klh.edu.in>
2025-08-02 09:42:25 +0530

    Add files via upload

---
+++ TrimWhiteSpace.java
@@ -0,0 +1,12 @@
+package oops_programs;
+
+public class TrimWhiteSpace {
+	public static void main(String[] args) {
+		String s = "  Hello  ";
+        System.out.println("[" + s.trim() + "]"); 
+	}
+	
+	    
+	}
+
+
