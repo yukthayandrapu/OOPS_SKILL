e30462da6960be0504cd3dc00782cdd3dcc4ea42
yukthayandrapu <2420080060@klh.edu.in>
2025-08-06 11:32:53 +0530

    Add files via upload

---
+++ CharLength.java
@@ -0,0 +1,11 @@
+package oops_programs;
+
+public class CharLength {
+	public static void main(String[] args) {
+		String s = "java programs";
+		System.out.println("charAt(2): " + s.charAt(2));  
+		System.out.println("length(): " + s.length());    
+	    }
+	}
+
+
