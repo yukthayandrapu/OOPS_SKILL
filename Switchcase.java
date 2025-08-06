272f7e5a054dd6bdb963857167f3636483731976
yukthayandrapu <2420080060@klh.edu.in>
2025-08-06 10:14:04 +0530

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,16 @@
+package oops_programs;
+
+public class Switchcase {
+	 public static void main(String[] args) {
+		  int day = 5;
+		  switch (day) {
+		  case 1: System.out.println("Monday"); break;
+		  case 2: System.out.println("Tuesday"); break;
+		  case 3: System.out.println("Wednesday"); break;
+		  case 4: System.out.println("Thursday"); break;
+		  default : System.out.println("Other day"); break;
+		  }
+		 }
+
+		}
+
